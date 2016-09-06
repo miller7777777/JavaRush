5ddf812c1dbe66b6bb0bfe2bd989144101db452d
miller777 <miller777@mail.ru>
2016-09-07 02:01:32 +0300

    level 20 task 2

--- src/com/javarush/test/level19/lesson03/task04/Solution.java
+++ src/com/javarush/test/level19/lesson03/task04/Solution.java
@@ -1,5 +1,6 @@
 package com.javarush.test.level19.lesson03.task04;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.Date;
 import java.util.GregorianCalendar;
@@ -9,43 +10,39 @@ import java.util.Scanner;
 Адаптировать Scanner к PersonScanner.
 Классом-адаптером является PersonScannerAdapter.
 Данные в файле хранятся в следующем виде:
-Иванов Иван Иванович 31 12 1950
-
-В файле хранится большое количество людей, данные одного человека находятся в одной строке. Метод read() должен читать данные одного человека.
+Иванов Иван Иванович 31 12 1978
+Подсказка: воспользуйтесь классом Calendar
 */
 
 public class Solution {
-    public static class PersonScannerAdapter implements PersonScanner{
 
-        Scanner scanner;
+    public static void main(String[] args) throws IOException {
+        Scanner s = new Scanner(new File("d:\\1.txt"));
+        PersonScanner adapter = new PersonScannerAdapter(s);
+        System.out.println(adapter.read());
+        adapter.close();
+    }
 
-        public PersonScannerAdapter(Scanner scanner)
-        {
+    public static class PersonScannerAdapter implements PersonScanner {
+        private Scanner scanner;
+
+        public PersonScannerAdapter (Scanner scanner) {
             this.scanner = scanner;
         }
 
         @Override
-        public Person read() throws IOException
-        {
-            String data = this.scanner.nextLine();
-            String[] dataOfPerson = data.split(" ");
-            String firstName = dataOfPerson[0];
-            String middleName = dataOfPerson[1];
-            String lastName = dataOfPerson[2];
-            int day = Integer.parseInt(dataOfPerson[3]);
-            int month = Integer.parseInt(dataOfPerson[4]);
-            int year = Integer.parseInt(dataOfPerson[5]) - 1;
-            GregorianCalendar calendar = new GregorianCalendar(year, month, day);
-            Date birthday = calendar.getTime();
-
-            return new Person(firstName, middleName, lastName, birthday);
+        public Person read() throws IOException {
+            if (scanner.hasNext()) {
+                String[] inputArray = scanner.nextLine().split(" ");
+                Date birthDate = (new GregorianCalendar(Integer.parseInt(inputArray[5]),Integer.parseInt(inputArray[4]) - 1, Integer.parseInt(inputArray[3]))).getTime();
+                return new Person(inputArray[1],inputArray[2],inputArray[0],birthDate);
+            }
+            return null;
         }
 
         @Override
-        public void close() throws IOException
-        {
-
-            this.scanner.close();
+        public void close() throws IOException {
+            scanner.close();
         }
     }
 }
